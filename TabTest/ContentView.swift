301e68441832d1953cf08be956be0e81643db06f
ZeeWanderer <mxx888777@gmail.com>
2020-11-18 12:20:28 -0800

    Initial Commit

---
+++ TabTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabTest
+//
+//  Created by Maksym Kulyk on 11/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabTest/TabTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabTestApp.swift
+//  TabTest
+//
+//  Created by Maksym Kulyk on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct TabTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
